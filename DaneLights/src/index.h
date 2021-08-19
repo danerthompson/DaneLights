525edc11bffc9e8d82c88009b5bf5ca4e0d3114b
danerthompson <58756948+danerthompson@users.noreply.github.com>
2021-08-19 17:53:22 -0500

    First commit!

---
+++ DaneLights/src/index.h
@@ -0,0 +1,75 @@
+//May need to use PROGMEM
+
+const char index_html[] = R"rawliteral(
+<!DOCTYPE HTML><html>
+
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/@jaames/iro@5"></script>
+  <style>
+    h1 {text-align: center;}
+    p {text-align: center;}
+    div {margin: auto;}
+  </style>
+</head>
+
+<body style="background-color:rgb(45, 45, 48);">
+
+<h1 style="color:white"> Dane's Lights </h1>
+<p style="color:white">Powered by ESP32</p>
+
+<div class="ColorPicker" id="defaultPicker" style="width=100%;height=300px;"></div>
+
+<p id="currentColor", style="color:white"> </p>
+
+
+<script>
+    var defaultPicker;
+    var ledColor;
+    var response;
+    var prevColor = 0;
+
+    readPreferences(); //Load color picker with preferences
+
+    setInterval( function() {
+        myFunction();
+    }, 100);
+
+
+    function myFunction() {
+
+        ledColor = defaultPicker.color.hexString;
+        if (ledColor != prevColor) {
+            document.getElementById("currentColor").innerHTML = ledColor;
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("GET", "/update?hex=" + ledColor.substring(1), true); //Have to remove first char from x, server detects '#' as delimiter
+            xhttp.send();
+            prevColor = ledColor;
+        }
+
+    }
+
+    function readPreferences() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                response = xhttp.responseText;
+                defaultPicker = new iro.ColorPicker("#defaultPicker", {
+                    width: 300,
+                    color: response,
+                    borderWidth: 1,
+                    borderColor: "#fff",
+                });
+            }
+        }
+        xhttp.open("GET", "/initialize", true);
+        xhttp.responseType = 'text';
+        xhttp.send();
+
+    }
+</script>
+
+</body>
+</html>
+)rawliteral";
---
+++ DaneLights/src/main.cpp
@@ -0,0 +1,99 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Preferences.h>
+#include "credentials.h" //Where userSSID, userPassword, and userPort are stored
+#include "index.h"
+
+// Replace with your network credentials
+const char* ssid = userSSID;
+const char* password = userPassword;
+
+//debugging stuff
+int count = 0;
+unsigned long timer;
+int r, g, b;
+
+Preferences preferences;
+
+// Create AsyncWebServer object on port user defined port
+AsyncWebServer server(userPort);
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  pinMode(23, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(21, OUTPUT);
+  
+  ledcSetup(0, 300, 8);
+  ledcSetup(1, 300, 8);
+  ledcSetup(2, 300, 8);
+
+  ledcAttachPin(14, 0);
+  ledcAttachPin(25, 1);
+  ledcAttachPin(32, 2);
+
+  timer = millis();
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+
+  server.on("/initialize", HTTP_GET, [](AsyncWebServerRequest *request) {
+
+    //Load current hex code
+    preferences.begin("lights", false);
+    Serial.println("initialize called");
+    String prevRGB = preferences.getString("prevRGB", "000000"); //RGB in hex
+    preferences.putString("prevRGB", prevRGB);
+    preferences.end();
+
+    request->send(200, "text/plain", prevRGB);
+  });
+
+  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String rgb = "No message";
+    char rgbConverted[6];
+    const char* param = "hex";
+
+    if (request->hasParam(param)) {
+      rgb = request->getParam(param)->value();
+    }
+
+    for (int i = 0; i < 6; i++) {
+      rgbConverted[i] = rgb[i];
+    }
+
+    //Store current hex code
+    preferences.begin("lights", false);
+    preferences.putString("prevRGB", rgb);
+    preferences.end();
+
+    sscanf(rgbConverted, "%02x%02x%02x", &r, &g, &b);
+
+    Serial.println(rgb);
+    Serial.print(r); Serial.print(" "); Serial.print(g); Serial.print(" "); Serial.println(b);
+
+    //Write to LEDs
+    ledcWrite(0, r);
+    ledcWrite(1, g);
+    ledcWrite(2, b);
+
+    request->send(200, "text/plain", "OK");
+  });
+  server.begin();
+}
+
+void loop() {
+}
